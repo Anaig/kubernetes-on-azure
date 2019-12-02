00951fe74922b7f06280e13e4cbff3c598f886d8
Anaig Marechal <ana.marechal@orange.fr>
2019-12-02 17:57:12 +0100

    add k8s creation script

--- Terraform-deployment/variables.tf
+++ Terraform-deployment/variables.tf
@@ -1,5 +1,34 @@
 variable "location" {}
-variable "resource_group_name" {
-    type = "string"
-    description = "Resource group name"
+variable "client_id" {}
+variable "client_secret" {}
+variable resource_group_name {}
+
+variable "agent_count" {
+    default = 3
+}
+
+variable "ssh_public_key" {
+    default = "~/.ssh/id_rsa.pub"
+}
+
+variable "dns_prefix" {
+    default = "k8stest"
+}
+
+variable cluster_name {
+    default = "k8stest"
+}
+
+variable log_analytics_workspace_name {
+    default = "testLogAnalyticsWorkspaceName"
+}
+
+# refer https://azure.microsoft.com/global-infrastructure/services/?products=monitor for log analytics available regions
+variable log_analytics_workspace_location {
+    default = "westeurope"
+}
+
+# refer https://azure.microsoft.com/pricing/details/monitor/ for log analytics pricing 
+variable log_analytics_workspace_sku {
+    default = "PerGB2018"
 }
